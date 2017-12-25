b509d57e7de67d67e8d5b6b5461dab4ec709dc95
Richard Saunders <richard@sdevsolutions.com>
2017-12-25 01:44:00 +0100

    Polls backend for data

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 <img src="./docs/desktop.screenshot.png" width="1024px" />
 
-### Setup
+### Development
 
 ```
 yarn install
@@ -19,67 +19,36 @@ npm install
 npm start
 ```
 
+### Build
+creates a static version of the app in the `build` directory
+
+```
+yarn install
+yarn build
+```
+or
+```
+npm install
+npm build
+```
+
+
 ### Data
 
-This \[will eventually\] call a back end service to supply it with data in the following formats. For now it just has
-hard coded data.
+This app calls a backend service to supply it with data in the following [format](./docs/example.json)
 
+By default the url to the backend is defined as the Environment Variable
 ```
-export const spotPriceData = {
-  BTC: {
-    GBP: 11015.06,
-    USD: 14715.00,
-  },
-  ETH: {
-    GBP: 535.23,
-    USD: 715.01,
-  },
-  LTC: {
-    GBP: 209.88,
-    USD: 280.84,
-  },
-  BCH: {
-    GBP: 2135.98,
-    USD: 2849.06,
-  },
-};
-
-export const currentHoldingsData = {
-   "Richy": {
-     "overallProfit": -44.1779087836,
-     "depositedValue": 300,
-     "BTC": {
-       "holds": 0.00518308,
-       "valueGBP": 51.9033112892
-     },
-     "ETH": {
-       "holds": 0.0186399,
-       "valueGBP": 9.033641136
-     },
-     "LTC": {
-       "holds": 1.00208319,
-       "valueGBP": 194.8851387912
-     }
-   },
-   "John": {
-     "overallProfit": 96.3166815564,
-     "depositedValue": 500,
-     "BTC": {
-       "holds": 0.05954836,
-       "valueGBP": 596.3166815564
-     },
-     "ETH": {
-       "holds": 0,
-       "valueGBP": 0
-     },
-     "LTC": {
-       "holds": 0,
-       "valueGBP": 0
-     }
-   },
-};
+REACT_APP_BACKEND_URL=https://binaryitch.co.uk/dev/coinbase/raw.php
+REACT_APP_BACKEND_POLL_INTERVAL=10000 //time between calls to the backend to update data (milliseconds)
+REACT_APP_LAST_UPDATED_INTERVAL=5000  //interval it informs you how long it's been since it last updated (milliseconds)
 ```
 
-### Responsive
+you can override these by creating an `.env.local` file.
+
+**Note:** you'll need to restart your development server when changing these values and/or rebuild the app for production.
+
+
+### Responsive example
 
 <img src="./docs/mobile.screenshot.png" />
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import Responsive from 'react-responsive';
+import { FormattedRelative } from 'react-intl';
 
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -9,31 +10,67 @@ import CurrentHoldingsTable from './components/CurrentHoldingsTable';
 import CurrentHoldingsTableMobile from './components/CurrentHoldingsTableMobile';
 import SpotPricesTable from './components/SpotPricesTable';
 
-import { spotPrices, currentHoldings } from './data/example'
-
 import './App.css';
 
 const Desktop = props => <Responsive {...props} minWidth={1024} />;
 const Mobile = props => <Responsive {...props} maxWidth={1024} />;
 
 class App extends Component {
+
+  state = {
+    currentHoldings: undefined,
+    spotPrices: undefined,
+  };
+
+  componentDidMount() {
+    this.fetchData();
+    setInterval(this.fetchData, process.env.REACT_APP_BACKEND_POLL_INTERVAL);
+  };
+
+  fetchData = () => fetch(process.env.REACT_APP_BACKEND_URL)
+    .then(res => res.json())
+    .then(({ currentHoldings, spotPrices }) => {
+      this.setState({
+        lastUpdated: Date.now(),
+        currentHoldings,
+        spotPrices,
+      });
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+
   render() {
+    const { currentHoldings, spotPrices, lastUpdated } = this.state;
     return (
       <MuiThemeProvider>
         <div className="App">
-          <Paper className="Paper">
-            <AppBar title="Spot Prices" showMenuIconButton={false} />
-            <SpotPricesTable data={spotPrices} />
-          </Paper>
-          <Paper className="Paper">
-            <AppBar title="Current Holdings" showMenuIconButton={false} />
-            <Desktop>
-              <CurrentHoldingsTable data={currentHoldings} />
-            </Desktop>
-            <Mobile>
-              <CurrentHoldingsTableMobile data={currentHoldings} />
-            </Mobile>
-          </Paper>
+          {spotPrices &&
+            <Paper className="Paper">
+              <AppBar
+                title={
+                  <Fragment>
+                    Spot Prices (Last updated{' '}
+                    <FormattedRelative
+                      value={lastUpdated}
+                      updateInterval={process.env.REACT_APP_LAST_UPDATED_INTERVAL}
+                    />)
+                  </Fragment>}
+                showMenuIconButton={false} />
+              <SpotPricesTable data={spotPrices}/>
+            </Paper>
+          }
+          {currentHoldings &&
+            <Paper className="Paper">
+              <AppBar title="Current Holdings" showMenuIconButton={false} />
+              <Desktop>
+                <CurrentHoldingsTable data={currentHoldings}/>
+              </Desktop>
+              <Mobile>
+                <CurrentHoldingsTableMobile data={currentHoldings}/>
+              </Mobile>
+            </Paper>
+          }
         </div>
       </MuiThemeProvider>
     );
